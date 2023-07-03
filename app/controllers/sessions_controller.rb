c3689a5911797bd450ca8181c577088a2dfaacbd
Cody <codybarker.or@gmail.com>
2023-07-03 12:43:42 -0700

    checking errors handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: {error: session.errors.full_messages}, status: :unauthorized
+            render json: {errors: session.errors.full_messages}, status: :unauthorized
         end
     end
 
